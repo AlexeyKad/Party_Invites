df14196e496ab229a1e0c51211bae03cd600bba8
Alexey <kadomtsef@gmail.com>
2017-07-26 13:21:51 +0300

    commit description

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Emil { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
